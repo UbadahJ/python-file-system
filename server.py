36274658d955f13c8ab017a216d7201c8de77fb6
LordChadiwala <ubadahjafry@gmail.com>
2021-01-06 21:17:51 +0500

    Update server launch script

--- server.py
+++ server.py
@@ -4,5 +4,10 @@ from server.headless import Server
 
 if __name__ == '__main__':
     _fs = FileSystem.load()
-    print(network.get_local_ip())
+    print("FS Manager Server, v0.1")
+    print("Local Ip Address", network.get_local_ip())
+    print("Public Ip Address", network.get_public_ip())
+    print("Port", 5500)
+    print("-" * 20)
+
     Server(id=1, port=5500, fs=_fs)
